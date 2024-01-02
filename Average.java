7861cd6292a4804ff2129bb79dc01f110ddaae44
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-02 19:01:27 +0530

    Average

---
+++ Average.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Average {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            int average = (A + B + C) / 3;
+            System.out.println("Average is : " + average);
+        }
+    }
+}
