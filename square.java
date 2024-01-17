ae892adc698d8168fef82205aba16f687fa2e2c7
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-17 15:02:11 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("****");
+        }
+    }
+}
