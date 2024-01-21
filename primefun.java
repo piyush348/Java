9aaaeafbf9170332d40ee1eff76718a3ebf01269
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-21 10:34:32 +0530

    Add files via upload

---
+++ primefun.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class primefun {
+    public static boolean isPrime(int n) {
+
+        boolean isPrime = true;
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(13));
+    }
+}
