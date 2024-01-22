3e89df4e4c0055068c79ea975fe8ebb37c69edc1
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-23 00:33:22 +0530

    Add files via upload

--- product.java
+++ product.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class product {
+    public static void product(int a, int b) {
+        int result = a * b;
+        System.out.println("Product is " + result);
+    }
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int product = a * b;
-            System.out.println(product);
-
+            product(a, b);
         }
     }
 }
