68211c428df41410e46cd67cc0b36c89cc9e562b
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-26 07:33:39 +0530

    Add files via upload

---
+++ HollowRect.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HollowRect {
+    public static void hollow_rectangle(int totRows, int totCols) {
+        // outer loop
+        for (int i = 1; i <= totRows; i++) {
+            // inner loop
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_rectangle(6, 4);
+    }
+}
