dfb721078c6b99fcdbfce94b9f7e674f10960b85
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-01-31 00:32:35 +0530

    Add files via upload

---
+++ Kadane.java
@@ -0,0 +1,20 @@
+public class Kadane {
+    public static void kadanes(int number[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for (int i = 0; i < number.length; i++) {
+            cs = cs + number[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("our max subarray sum is :" + ms);
+    }
+
+    public static void main(String[] args) {
+        int number[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kadanes(number);
+    }
+}
