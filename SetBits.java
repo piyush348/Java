af22afbb43621cf4a73357558a73e7694dcfbf8b
PIYUSH CHATURVEDI <118364730+piyush348@users.noreply.github.com>
2024-02-04 00:01:36 +0530

    Add files via upload

---
+++ SetBits.java
@@ -0,0 +1,17 @@
+
+public class SetBits {
+    public static int countsetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countsetBits(10));
+    }
+}
